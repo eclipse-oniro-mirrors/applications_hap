934bd4addcb56b9d9ea57672cbc3c83bb021aed0
xietingwei <xietingwei@h-partners.com>
2026-07-08 16:40:11 +0800

    修改编译脚本，解决找不到uv问题
    Signed-off-by: xietingwei <xietingwei@h-partners.com>

--- build.sh
+++ build.sh
@@ -142,7 +142,30 @@ if [ "$arg_build_sdk" == "true" ]; then
 fi
 
 
-export PATH=/home/tools/command-line-tools/ohpm/bin:$PATH
+# set python3
+case $(uname -m) in
+	arm64|aarch64)
+		host_cpu_prefix=arm64
+		;;
+	*)
+		host_cpu_prefix=x86
+		;;
+esac
+
+PYTHON3_DIR=$(realpath ${ROOT_PATH}/prebuilts/python/linux-${host_cpu_prefix}/* | tail -1)
+PYTHON3=${PYTHON3_DIR}/bin/python3
+PYTOHN=${PYTOHN3_DIR}/bin/python
+
+if [[ ! -f "${PYTHON}" ]]; then
+	echo -e "\033[31m[OHOS ERROR] Please execute the build/prebuilts_download.sh \033[0m"
+	exit 1
+else
+	if [[ ! -f "${PYTHON}" ]]; then
+		ln -sf "${PYTHON3}" "${PYTHON}"
+	fi
+fi
+
+export PATH=${ROOT_PATH}/prebuilts/build-tools/linux-${host_cpu_prefix}/bin:${PYTHON3_DIR}/bin:/home/tools/command-line-tools/ohpm/bin:$PATH
 npm config set registry https://repo.huaweicloud.com/repository/npm/
 npm config set @ohos:registry https://repo.harmonyos.com/npm/
 npm config set strict-ssl false
